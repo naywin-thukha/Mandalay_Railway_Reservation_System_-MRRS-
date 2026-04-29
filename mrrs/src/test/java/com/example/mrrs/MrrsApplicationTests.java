b7311d5dea0e0790787265a5f4cdb9e27f6af1e5
Thu Kha Nay Win <thukhanaywin@gmail.com>
2026-04-29 14:47:17 +0630

    uploaded Spring File

---
+++ mrrs/src/test/java/com/example/mrrs/MrrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mrrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
