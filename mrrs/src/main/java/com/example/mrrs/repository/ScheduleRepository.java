2f0753c4833c13d457862b1f633dc42cff956f30
Thu Kha Nay Win <thukhanaywin@gmail.com>
2026-05-06 14:37:19 +0630

    add schedule repository

---
+++ mrrs/src/main/java/com/example/mrrs/repository/ScheduleRepository.java
@@ -0,0 +1,14 @@
+package com.example.mrrs.repository;
+
+import com.example.mrrs.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findByRoute_RouteIdAndTravelDate(Long routeId, LocalDate travelDate);
+
+    List<Schedule> findByTrain_TrainId(Long trainId);
+}
